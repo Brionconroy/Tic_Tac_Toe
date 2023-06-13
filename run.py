28d79fe1c671e9f991ea7317e427ab503ee5089f
Brionconroy <razvanbrion@gmail.com>
2023-06-13 11:05:29 +0000

    Tidy up code using python validator

--- run.py
+++ run.py
@@ -13,7 +13,9 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('tic_tac_toe')
 
-coordinate = {1 : "1", 2 : "2", 3 : "3", 4 : "4", 5 : "5", 6 : "6", 7 : "7", 8 : "8", 9 : "9"}
+coordinate = {1: "1", 2: "2", 3: "3", 4: "4",
+              5: "5", 6: "6", 7: "7", 8: "8", 9: "9"}
+
 
 def welcome_message():
     """
@@ -26,6 +28,7 @@ def welcome_message():
     print("To win join three counters in a row before the computer")
     print("All spaces on the board taken up be counters will result in a draw")
     print("To exit the game at any time press 'e")
+
 
 def get_user_name_1():
     """
@@ -41,7 +44,8 @@ def get_user_name_1():
             break
     return username
 
-    def get_user_name_2():
+
+def get_user_name_2():
     """
     Get users name and make sure that there is input validation.
     """
@@ -62,15 +66,18 @@ def print_board(coordinate):
     Using f string syntax to be able to update the board.
     """
     game_board = (f"|{coordinate[1]}|{coordinate[2]}|{coordinate[3]}|\n"
-                    f"|{coordinate[4]}|{coordinate[5]}|{coordinate[6]}|\n"
-                    f"|{coordinate[7]}|{coordinate[8]}|{coordinate[9]}|\n"
-                    )
+                  f"|{coordinate[4]}|{coordinate[5]}|{coordinate[6]}|\n"
+                  f"|{coordinate[7]}|{coordinate[8]}|{coordinate[9]}|\n")
     print(game_board)
+
 
 def check_turn(turn):
 
-    if turn % 2 == 0: return "O"
-    else: return "X"
+    if turn % 2 == 0:
+        return "O"
+    else:
+        return "X"
+
 
 def winning_conditions(coordinate):
     """
@@ -78,24 +85,30 @@ def winning_conditions(coordinate):
     """
     if (coordinate[1] == coordinate[5] == coordinate[9]) \
        or (coordinate[3] == coordinate[5] == coordinate[7]):
-       return True
+        return True
     elif (coordinate[1] == coordinate[4] == coordinate[7]) \
-       or (coordinate[2] == coordinate[5] == coordinate[8]) \
-       or (coordinate[3] == coordinate[6] == coordinate[9]):
-       return True
+        or (coordinate[2] == coordinate[5] == coordinate[8]) \
+            or (coordinate[3] == coordinate[6] == coordinate[9]):
+        return True
     elif (coordinate[1] == coordinate[2] == coordinate[3]) \
-       or (coordinate[4] == coordinate[5] == coordinate[6]) \
-       or (coordinate[7] == coordinate[8] == coordinate[9]):
-       return True
-    else: return False
+        or (coordinate[4] == coordinate[5] == coordinate[6]) \
+            or (coordinate[7] == coordinate[8] == coordinate[9]):
+        return True
+    else:
+        return False
+
 
 playing_game = True
 
+
 end_game = False
+
 
 turn = 0
 
+
 last_turn = -1
+
 
 welcome_message()
 get_user_name_1()
@@ -104,13 +117,14 @@ get_user_name_2()
 
 while playing_game:
     # Keeps the same board on the screen.
-    os.system("cls" if os.name == "nt" else "clear") # This code was taken from CScode
+    # This code was taken from CScode
+    os.system("cls" if os.name == "nt" else "clear")
     print_board(coordinate)
     # Print statment of an invalid trun.
     if last_turn == turn:
-        print("Someone has already gone there. pick another coordinate and try again")
+        print("Spot taken! Pick another spot and try again")
     last_turn = turn
-    print("Player " + str((turn % 2) + 1) + "'s turn: Pick your coordinate on the board")
+    print("Player " + str((turn % 2) + 1) + "'s turn: Pick your spot.")
     # Player input.
     users_turn = input()
     # Exit the game.
@@ -121,24 +135,27 @@ while playing_game:
     elif str.isdigit(users_turn) and int(users_turn) in coordinate:
         # Make sure spot has not been taken already.
         if not coordinate[int(users_turn)] in {"X", "O"}:
-            # Validates input to the board and updates the board, if valid input.
-              turn += 1
-              coordinate[int(users_turn)] = check_turn(turn)
+            # Validates input to the board and updates the board.
+            turn += 1
+        coordinate[int(users_turn)] = check_turn(turn)
         # Check for a win.
-    if winning_conditions(coordinate): playing_game, end_game = False, True
+    if winning_conditions(coordinate):
+        playing_game, end_game = False, True
     # Draw result
-    if turn > 8: playing_game = False
+    if turn > 8:
+        playing_game = False
 
-os.system("cls" if os.name == "nt" else "clear") # This code was taken from CScode
+# This code was taken from CScode
+os.system("cls" if os.name == "nt" else "clear")
 print_board(coordinate)
 
 if end_game:
-    if check_turn(turn) == "X": print("player 1 wins")
-    else: print("player 2 wins")
+    if check_turn(turn) == "X":
+        print("player 1 wins")
+    else:
+        print("player 2 wins")
 else:
     # Draw result.
     print("It's a Draw!")
 
 print("Good Game!")
-
-
